8d23aa0f1abbd8e7c5e02d93b92123ad220165fa
Matheus Albuquerque <land@ythecombinator.space>
2020-12-13 06:34:38 +0100

    feat(src/shared/utils): regexes

---
+++ src/shared/utils/regex.ts
@@ -0,0 +1,11 @@
+// Checks if at least one lower case letter exists
+export const lowercase = /(?=.*[a-z])/;
+
+// Checks if at least one upper case letter exists
+export const uppercase = /(?=.*[A-Z])/;
+
+// Checks if at least one digit exists
+export const digit = /(?=.*\d)/;
+
+// Checks if at least one non-word exists
+export const specialSymbol = /(?=.*\W)/;
